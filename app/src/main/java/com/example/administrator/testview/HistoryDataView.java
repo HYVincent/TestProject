965b152345a5d1c5e60cb70726da7cca86021f57
Vincent <1032006226@qq.com>
2018-08-13 17:54:24 +0800

    解决模式切换bug,全局变量慎用！！！！

--- app/src/main/java/com/example/administrator/testview/HistoryDataView.java
+++ app/src/main/java/com/example/administrator/testview/HistoryDataView.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.graphics.CornerPathEffect;
 import android.graphics.DashPathEffect;
 import android.graphics.LinearGradient;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PathEffect;
@@ -54,6 +55,8 @@ public class HistoryDataView extends BaseScrollerView {
         mViewWidth = w;
         mViewHeight = h;
 //        Log.d(TAG, "onSizeChanged222: 【"+mViewWidth+"】"+"【"+mViewHeight+"】");
+//        mBitmap = Bitmap.createBitmap((int) mViewWidth,(int) mViewHeight, Bitmap.Config.ARGB_8888);
+//        mCanvas = new Canvas(mBitmap);
         super.onSizeChanged(w, h, oldw, oldh);
     }
 
@@ -71,13 +74,11 @@ public class HistoryDataView extends BaseScrollerView {
     //水分值画笔
     private Paint mMoistureValuePaint;
     //第一条线距离文字的距离
-    private float mFirstLineMarginTop = 20;
+    private float mFirstLineMarginTop = 40;
     //从上往下最后一条线到底部的距离
     private float mEndLineMarginButtom = 30;
     //白皙度path
-    private Path mWhiteDegreePath;
-    //水分值
-    private Path mMoistureValuePath;
+//    private Path mWhiteDegreePath;
     //两条线之间的间距
     private float mItemLineMargin;
     //顶部tag文字的高度
@@ -87,7 +88,7 @@ public class HistoryDataView extends BaseScrollerView {
     //两个数据点之间的距离
     private float mItemDataMargin = 60;
     //数据
-    private List<DataBean> dataBeans = new ArrayList<>();
+    private List<DataBean> dataBeans;
     //当前选择的类型 分为三种 0-->日 1-->周 2-->月 一周或者一月中的某天有多个数据时取平均值
     //类型为 1或者2 的时候都是显示为 08/06 格式，否则显示 14：55
     private int timeType = 0;
@@ -103,24 +104,24 @@ public class HistoryDataView extends BaseScrollerView {
     private float mPercentage;
     //true 填充下面 false 不填充
 //    private boolean isFillDownLineColor;
-    private Path path;
-    private Path path2;
+//    private Path path;
+//    private Path path2;
     private Paint mCircleDotPaint;
     private Paint rectPaint;
     private Point[] circlePoint;
-    //曲线点集合
-//    private Point[] linePoint;
+
+    private Matrix matrix;
 
     /*设置数据*/
-    public void setDataBeans(List<DataBean> dataBeans) {
-        this.dataBeans = dataBeans;
-        mOffsetX = 0;
-        mPercentage = 0;
-        mMaxOffsetX = 0;
+    public void setDataBeans(List<DataBean> datas) {
+        this.dataBeans.clear();
+        if (datas != null && datas.size() > 0) {
+            this.dataBeans.addAll(datas);
+        }
         invalidate();
     }
 
-    public void setTimeType(int timeType){
+    public void setTimeType(int timeType) {
         this.timeType = timeType;
         invalidate();
     }
@@ -131,19 +132,24 @@ public class HistoryDataView extends BaseScrollerView {
      */
     public void changeModel(boolean isBXD) {
         this.isBXD = isBXD;
+        mOffsetX = 0;
+        mPercentage = 0;
+        mMaxOffsetX = 0;
         invalidate();
     }
 
     private void init(Context context, AttributeSet attrs) {
         this.mContext = context;
+        dataBeans = new ArrayList<>();
+        matrix = new Matrix();
         mTopTag = context.getString(R.string.string_history_tag_white_degree);
         mTopTagMaraginLeft = DpUtil.dp2px(mContext, mTopTagMaraginLeft);
         mFirstLineMarginTop = DpUtil.dp2px(mContext, mFirstLineMarginTop);
         mBgLineMargin = DpUtil.dp2px(mContext, mBgLineMargin);
         mEndLineMarginButtom = DpUtil.dp2px(mContext, mEndLineMarginButtom);
         mItemDataMargin = DpUtil.dp2px(mContext, mItemDataMargin);
-        mStartDataX = DpUtil.dp2px(mContext,mStartDataX);
-        mEndDataMaginRight = DpUtil.dp2px(mContext,mEndDataMaginRight);
+        mStartDataX = DpUtil.dp2px(mContext, mStartDataX);
+        mEndDataMaginRight = DpUtil.dp2px(mContext, mEndDataMaginRight);
 
         mTagPaint = new Paint();
         mTagPaint.setColor(ContextCompat.getColor(mContext, R.color.color_black_2a2a2a));
@@ -155,10 +161,6 @@ public class HistoryDataView extends BaseScrollerView {
         mBgLinePaint.setStrokeWidth(DpUtil.dp2px(mContext, 1) / 3);
         mBgLinePaint.setAntiAlias(true);
 
-        mWhiteDegreePath = new Path();
-        mMoistureValuePath = new Path();
-        path = new Path();
-        path2 = new Path();
 
         mWhiteDegreePaint = new Paint();
         mWhiteDegreePaint.setColor(ContextCompat.getColor(mContext, R.color.color_red_fa73a2));
@@ -170,7 +172,7 @@ public class HistoryDataView extends BaseScrollerView {
         mWhiteDegreePaint.setPathEffect(pe);
 
         mCircleDotPaint = new Paint();
-        mCircleDotPaint.setStrokeWidth(DpUtil.dp2px(mContext,2));
+        mCircleDotPaint.setStrokeWidth(DpUtil.dp2px(mContext, 2));
         mCircleDotPaint.setColor(ContextCompat.getColor(mContext, R.color.color_red_FF6347));
         mCircleDotPaint.setXfermode(new PorterDuffXfermode(
                 PorterDuff.Mode.SRC_OVER));
@@ -187,19 +189,22 @@ public class HistoryDataView extends BaseScrollerView {
 
         rectPaint = new Paint();
 //        rectPaint.setColor(ContextCompat.getColor(mContext, R.color.color_black_000000));
-        rectPaint.setStrokeWidth(DpUtil.dp2px(mContext,8));
-        rectPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_ATOP));
+        rectPaint.setStrokeWidth(DpUtil.dp2px(mContext, 8));
+        rectPaint.setXfermode(new PorterDuffXfermode(android.graphics.PorterDuff.Mode.SRC_ATOP));
     }
 
     private static final String TAG = "历史数据";
 
 
+    private Canvas canvas;
+
     @Override
-    protected void onDrawContent(Canvas canvas) {
-        if(mMaxOffsetX == 0 ){
-            mMaxOffsetX = mStartDataX+(dataBeans.size()-1) * mItemDataMargin - mViewWidth + mEndDataMaginRight;
+    protected void onDrawContent(Canvas mCanvas) {
+        mCanvas.drawColor(Color.WHITE, PorterDuff.Mode.SRC_OVER);
+        if (mMaxOffsetX == 0 && dataBeans.size() > 0) {
+            mMaxOffsetX = mStartDataX + (dataBeans.size() - 1) * mItemDataMargin - mViewWidth + mEndDataMaginRight;
 //            Log.d(TAG, "onSizeChanged333:【"+mItemDataMargin+"】"+"【"+mViewWidth+"】"+"【"+mStartDataX+"】"+"【"+mMaxOffsetX+"】"+"【"+mEndDataMaginRight+"】");
-            setMaxVal((int)mMaxOffsetX);
+            setMaxVal((int) mMaxOffsetX);
         }
         if (isBXD) {
             mTopTag = mContext.getString(R.string.string_history_tag_white_degree);
@@ -208,19 +213,37 @@ public class HistoryDataView extends BaseScrollerView {
 //            绘制水分值 0~60 0 20 30 40 50 60 小于20的时候就不保存 大于60=60
         }
         //不需要移动的
-        drawTopTag(canvas);
-        drawBgLine(canvas);
+        drawTopTag(mCanvas);
+        drawBgLine(mCanvas);
         //保存和平移
-        canvas.save();
-        if(mStartDataX + (dataBeans.size()-1) * mItemDataMargin > mViewWidth){
-            canvas.translate(mOffsetX,0);
-        }
+        mCanvas.save();
+        if (dataBeans.size() > 0) {
+            if (mStartDataX + (dataBeans.size() - 1) * mItemDataMargin > mViewWidth) {
+                mCanvas.translate(mOffsetX, 0);
+            }
 //        Log.d(TAG, "drawWhiteDegree: 滑动偏移量-->【"+mOffsetX+"】");
-        // 绘制白皙度 0~100 0 2 4 6 8 10 小于20的时候不需要
-        //绘制数据内容
-        drawWhiteDegreeDetail(canvas);
-        //重置
-        canvas.restore();
+            // 绘制白皙度 0~100 0 2 4 6 8 10 小于20的时候不需要
+            //绘制数据内容
+            if (dataBeans.size() == 1) {
+                Rect valueRect = new Rect();
+                String valueStr;
+                if (isBXD) {
+                    valueStr = String.valueOf(dataBeans.get(0).getWhiteDegreeValue());
+                } else {
+                    valueStr = String.valueOf(dataBeans.get(0).getMoistureValue());
+                }
+                mValuePaint.getTextBounds(valueStr, 0, valueStr.length(), valueRect);
+                mCanvas.drawText(valueStr, mStartDataX - valueRect.width() / 2,
+                        getWhiteDegreeY(dataBeans.get(0)) - DpUtil.dp2px(mContext, 5) - valueRect.height(), mValuePaint);
+                mCanvas.drawCircle(mStartDataX, getWhiteDegreeY(dataBeans.get(0)), DpUtil.dp2px(mContext, 3), mCircleDotPaint);
+            } else {
+                drawWhiteDegreeDetail(mCanvas);
+            }
+        } else {
+            Log.e(TAG, "onDrawContent: dataBeas is null");
+        }
+        mCanvas.restore();
+        this.canvas = mCanvas;
     }
 
     private void drawTopTag(Canvas canvas) {
@@ -263,12 +286,13 @@ public class HistoryDataView extends BaseScrollerView {
         /*绘制白皙度 0~100 0 2 4 6 8 10 小于20的时候不需要*/
         //当值超过屏幕的时候需要可以滑动
         //两个数值之间的距离
-        mWhiteDegreePath.reset();
         //绘制地下的文字
+        Path path = new Path();
+        Path path2 = new Path();
         if (dataBeans.size() > 0) {
-            float startX =  mStartDataX;
-            float startY =  mViewHeight - mEndLineMarginButtom;
-            path.moveTo(startX,startY);
+            float startX = mStartDataX;
+            float startY = mViewHeight - mEndLineMarginButtom;
+            path.moveTo(startX, startY);
             int count = dataBeans.size();
             circlePoint = new Point[count];
 //            Log.d(TAG, "drawWhiteDegreeDetail: 共【"+count+"】个数据");
@@ -277,13 +301,13 @@ public class HistoryDataView extends BaseScrollerView {
                 // x,y表示当前点  x4,y4表示下一个点 x2,x3都是属于中间的点
                 x = startX + mItemDataMargin * i;
                 y = getWhiteDegreeY(dataBeans.get(i));
-                if(y > minY){
+                if (y > minY) {
                     minY = y;
                 }
-                if(i == count -1){
+                if (i == count - 1) {
                     x4 = x;
                     y4 = y;
-                }else {
+                } else {
                     x4 = startX + mItemDataMargin * (i + 1);
                     y4 = getWhiteDegreeY(dataBeans.get(i + 1));
                 }
@@ -295,29 +319,29 @@ public class HistoryDataView extends BaseScrollerView {
                     // 形成封闭的图形
                     path2.moveTo(x, y);
                     path.moveTo(x, startY);
-                    path.lineTo(x,startY);
+                    path.lineTo(x, startY);
                     path.lineTo(x, y);
                     //下面这一行不写，则表的左上角有个空白，不知道为啥
                     path.lineTo(x, y);
                 }
-                if(i == count -1){
-                    path.lineTo(x,startY);
-                    path2.lineTo(x,startY);
-                }else {
+                if (i == count - 1) {
+                    path.lineTo(x, startY);
+                    path2.lineTo(x, startY);
+                } else {
                     path.cubicTo(x2, y2, x3, y3, x4, y4);
                     path2.cubicTo(x2, y2, x3, y3, x4, y4);
-                    Log.d(TAG, "drawWhiteDegreeDetail: 白皙度y2-->"+y2+"   y3-->"+y3+"  y4-->"+y4);
+                    Log.d(TAG, "drawWhiteDegreeDetail: 白皙度y2-->" + y2 + "   y3-->" + y3 + "  y4-->" + y4);
                 }
 //                canvas.drawCircle(x, y, DpUtil.dp2px(mContext, 3), mCircleDotPaint);
-                circlePoint[i] = new Point((int) x,(int) y);
+                circlePoint[i] = new Point((int) x, (int) y);
                 //绘制时间
-                drawButtomTime(dataBeans.get(i),canvas,circlePoint[i]);
+                drawButtomTime(dataBeans.get(i), canvas, circlePoint[i]);
             }
             // 形成封闭的图形
-            path.lineTo(startX + (dataBeans.size()-1) * mItemDataMargin, startY);
+            path.lineTo(startX + (dataBeans.size() - 1) * mItemDataMargin, startY);
             path.close();
 
-            float left = startX-DpUtil.dp2px(mContext,3);
+            float left = startX - DpUtil.dp2px(mContext, 3);
             float top = getPaddingTop();
             float right = startX + dataBeans.size() * mItemDataMargin;
             float bottom = startY;
@@ -327,42 +351,43 @@ public class HistoryDataView extends BaseScrollerView {
             rectPaint.setShader(lg);
             canvas.drawPath(path, mWhiteDegreePaint);
             canvas.drawRect(left, top, right, bottom, rectPaint);
-            for (int i =0;i<circlePoint.length;i++){
+            for (int i = 0; i < circlePoint.length; i++) {
                 Rect valueRect = new Rect();
                 String valueStr;
-                if(isBXD){
+                if (isBXD) {
                     valueStr = String.valueOf(dataBeans.get(i).getWhiteDegreeValue());
-                }else {
+                } else {
                     valueStr = String.valueOf(dataBeans.get(i).getMoistureValue());
                 }
                 mValuePaint.getTextBounds(valueStr, 0, valueStr.length(), valueRect);
                 canvas.drawText(valueStr, circlePoint[i].x - valueRect.width() / 2,
                         circlePoint[i].y - DpUtil.dp2px(mContext, 5) - valueRect.height(), mValuePaint);
-                canvas.drawCircle(circlePoint[i].x,circlePoint[i].y,DpUtil.dp2px(mContext,3),mCircleDotPaint);
+                canvas.drawCircle(circlePoint[i].x, circlePoint[i].y, DpUtil.dp2px(mContext, 3), mCircleDotPaint);
             }
         }
     }
 
     /**
      * 绘制底部的文字
+     *
      * @param dataBean
      * @param canvas
      * @param point
      */
     private void drawButtomTime(DataBean dataBean, Canvas canvas, Point point) {
         String dateStr;
-        if(timeType != 0){
+        if (timeType != 0) {
             //按日期绘制
-            dateStr = DateUtil.getDateString(DateUtil.DATA_FORMAT_MONTH_DAY,dataBean.getTime());
-        }else {
+            dateStr = DateUtil.getDateString(DateUtil.DATA_FORMAT_MONTH_DAY, dataBean.getTime());
+        } else {
             //按时间绘制
-            dateStr = DateUtil.getDateString(DateUtil.DATE_FORMAT_HM,dataBean.getTime());
-        } //f
+            dateStr = DateUtil.getDateString(DateUtil.DATE_FORMAT_HM, dataBean.getTime());
+        }
         Rect rect = new Rect();
-        mValuePaint.getTextBounds(dateStr,0,dateStr.length(),rect);
-        mValuePaint.setColor(ContextCompat.getColor(mContext,R.color.color_gray_9fa1a9));
-        canvas.drawText(dateStr,point.x-rect.width()/2,mViewHeight - mEndLineMarginButtom/2 + rect.height()/2,mValuePaint);
-        mValuePaint.setColor(ContextCompat.getColor(mContext,R.color.color_black_000000));
+        mValuePaint.getTextBounds(dateStr, 0, dateStr.length(), rect);
+        mValuePaint.setColor(ContextCompat.getColor(mContext, R.color.color_gray_9fa1a9));
+        canvas.drawText(dateStr, point.x - rect.width() / 2, mViewHeight - mEndLineMarginButtom / 2 + rect.height() / 2, mValuePaint);
+        mValuePaint.setColor(ContextCompat.getColor(mContext, R.color.color_black_000000));
     }
 
     /**
@@ -372,12 +397,13 @@ public class HistoryDataView extends BaseScrollerView {
      * @return 白皙度最大值是 100 为最大值
      */
     private float getWhiteDegreeY(DataBean dataBean) {
-        if(isBXD){
-            Log.d(TAG, "getWhiteDegreeY: 白皙度数据");
-            return mViewHeight - mEndLineMarginButtom - mItemLineMargin * 5 * dataBean.getWhiteDegreeValue() / 100;
-        }else {
-            Log.d(TAG, "getWhiteDegreeY: 水分值数据-->");
-            return mViewHeight - mEndLineMarginButtom - mItemLineMargin * 5 * dataBean.getMoistureValue() / 60;
+        float y = 0;
+        if (isBXD) {
+            y = mViewHeight - mEndLineMarginButtom - mItemLineMargin * 5 * dataBean.getWhiteDegreeValue() / 100;
+            return y > mFirstLineMarginTop ? y : mFirstLineMarginTop;
+        } else {
+            y = mViewHeight - mEndLineMarginButtom - mItemLineMargin * 5 * dataBean.getMoistureValue() / 60;
+            return y > mFirstLineMarginTop ? y : mFirstLineMarginTop;
         }
     }
 
@@ -390,19 +416,5 @@ public class HistoryDataView extends BaseScrollerView {
         //绘制水分值 0~60 0 20 30 40 50 60 小于20的时候就不保存 大于60=60
 
     }
-
-
- /*   @Override
-    public void onScale(ScaleDetector detector) {
-//        super.onScale(detector);
-        if (mOffsetX == 0 && super.mMaxVal != 0){
-            mPercentage = -((detector.centerX - mStartDataX) / (super.mMaxVal + mViewWidth));
-        }else {
-            mPercentage = super.mMaxVal == 0 ? 0 : mOffsetX / super.mMaxVal;
-        }
-        mOffsetX = mPercentage == 0 ? 0 : super.mMaxVal * mPercentage;
-    }*/
-
-
 
 }
